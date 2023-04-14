4ed3348bb9e2c44ce22b728ff3db06b76fe6ea4a
Narek Khachatryan <65866030+BiNARyBeast0001@users.noreply.github.com>
2023-04-14 20:49:14 +0400

    Create Ian Visits Mary.cpp
    Solution to codeforces 1816A. https://codeforces.com/contest/1816/problem/A

---
+++ Ian Visits Mary.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(i=a;i<b;i++)
+#define fi(i,a,b) for(i=a;i>=b;i--)
+#define all(v) (v).begin(),(v).end()
+#define vi(v) vector<int> v
+#define vii(v) vector<ll> v
+#define srt(v) sort(v, v + n)
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+using ll = long long;
+//solution
+void solve()
+{
+    int a, b; cin >> a >> b;
+    cout << 2 << endl;
+    cout << a - 1 << " " << 1 << endl;
+    cout << a << " " << b << endl;
+    return;
+}
+int main()
+{
+    int t1;
+    t1 = 1;
+    cin >> t1;
+    while (t1--)
+        solve();
+    return 0;
+}
+ 
